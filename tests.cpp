6d52abc3f69310cba44be08a092566d574c85b07
A Student <student@nowhere.edu>
2026-03-16 16:10:05 +0000

    Mar 16, 2026, 9:10 AM

--- tests.cpp
+++ tests.cpp
@@ -4,4 +4,10 @@
 
 using doctest::Approx;
 
-TEST_CASE
+TEST_CASE("findMax") {
+  std::vector<double> sample1 = {12,6,4,9,20,16};
+  CHECK(findMax(sample1) == 20);
+
+  std::vector<double> sample2 = {2,6,4,9,20,16,25,12,14};
+  CHECK(findMax(sample2) == 25);
+}
